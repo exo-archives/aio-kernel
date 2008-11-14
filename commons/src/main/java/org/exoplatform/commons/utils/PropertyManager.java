ff8363d28b86525564d0b7d9ee65109b4d3c90d5
Julien Viet <julien.viet@exoplatform.com>
2008-11-14 13:25:06 +0000

    KER-41: PropertyManager for obtaining and managing system properties

--- commons/src/main/java/org/exoplatform/commons/utils/PropertyManager.java
+++ commons/src/main/java/org/exoplatform/commons/utils/PropertyManager.java
@@ -24,6 +24,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * if the property exo.product.developing is set to the false string. The cache usage is read once during the static
  * initialization of the cache and it can be programmatically triggered by calling the {@link #refresh()} method.
  *
+ * @todo add security privileged blocks used when eXo is executed under a security manager
+ *
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
  */
