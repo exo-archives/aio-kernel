7b8f9c97dba43d47b345424fe410ae2e315bd625
Julien Viet <julien.viet@exoplatform.com>
2009-04-01 07:15:26 +0000

    KER-86 : IOUtil improvements

--- commons/src/main/java/org/exoplatform/commons/utils/IOUtil.java
+++ commons/src/main/java/org/exoplatform/commons/utils/IOUtil.java
@@ -24,6 +24,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -33,61 +34,164 @@ import java.net.URL;
  * @email: tuan08@yahoo.com
  */
 public class IOUtil {
-  static public String getFileContenntAsString(File file, String encoding) throws Exception {
+
+  /** The buffer size for reading input streams. */
+  private static final int DEFAULT_BUFFER_SIZE = 256;
+
+  /**
+   * Returns the content of the specified file as a string using the <code>UTF-8</code> charset.
+   *
+   * @param file the file
+   * @return the content
+   * @throws IOException any io exception
+   * @throws NullPointerException if any argument is null
+   */
+  static public String getFileContentAsString(File file) throws IOException, NullPointerException {
+    return getFileContentAsString(file, "UTF-8");
+  }
+
+  /**
+   * Returns the content of the specified file as a string using the specified charset.
+   *
+   * @param file the file
+   * @param charset the charset
+   * @return the content
+   * @throws IOException any io exception
+   * @throws NullPointerException if any argument is null
+   */
+  static public String getFileContentAsString(File file, String charset) throws IOException, NullPointerException {
+    if (file == null) {
+      throw new NullPointerException("No null file accepted");
+    }
     FileInputStream is = new FileInputStream(file);
-    return new String(getStreamContentAsBytes(is), encoding);
+    return new String(getStreamContentAsBytes(is), charset);
   }
 
-  static public String getFileContenntAsString(File file) throws Exception {
-    FileInputStream is = new FileInputStream(file);
-    return new String(getStreamContentAsBytes(is));
+  /**
+   * Returns the content of the specified file as a string using the specified charset.
+   *
+   * @param fileName the file name
+   * @param charset the charset
+   * @return the content
+   * @throws IOException any io exception
+   * @throws NullPointerException if any argument is null
+   */
+  static public String getFileContentAsString(String fileName, String charset) throws IOException, NullPointerException {
+    if (fileName == null) {
+      throw new NullPointerException("No null file name accepted");
+    }
+    return getFileContentAsString(new File(fileName), charset);
   }
 
-  static public String getFileContenntAsString(String fileName, String encoding) throws Exception {
-    FileInputStream is = new FileInputStream(fileName);
-    return new String(getStreamContentAsBytes(is), encoding);
+  /**
+   * Returns the content of the specified file as a string using the <code>UTF-8<code> charset.
+   *
+   * @param fileName the file name
+   * @return the content
+   * @throws IOException any io exception
+   * @throws NullPointerException if any argument is null
+   */
+  static public String getFileContentAsString(String fileName) throws IOException, NullPointerException {
+    return getFileContentAsString(fileName, "UTF-8");
   }
 
-  static public String getFileContenntAsString(String fileName) throws Exception {
-    FileInputStream is = new FileInputStream(fileName);
-    return new String(getStreamContentAsBytes(is));
-  }
-
-  static public byte[] getFileContentAsBytes(String fileName) throws Exception {
-    FileInputStream is = new FileInputStream(fileName);
-    return getStreamContentAsBytes(is);
-  }
-
-  static public String getStreamContentAsString(InputStream is) throws Exception {
-    byte buf[] = new byte[is.available()];
-    is.read(buf);
+  /**
+   * Returns the content of the specified stream as a string using the <code>UTF-8</code> charset.
+   *
+   * @param is the stream
+   * @return the content
+   * @throws IOException any io exception
+   * @throws NullPointerException if the specified stream is null
+   */
+  static public String getStreamContentAsString(InputStream is) throws IOException, NullPointerException {
+    byte buf[] = getStreamContentAsBytes(is);
     return new String(buf, "UTF-8");
   }
 
-  static public byte[] getStreamContentAsBytes(InputStream is) throws Exception {
-    BufferedInputStream buffer = new BufferedInputStream(is);
-    ByteArrayOutputStream output = new ByteArrayOutputStream();
-    byte[] data = new byte[buffer.available()];
-    int available = -1;
-    while ((available = buffer.read(data)) > -1) {
-      output.write(data, 0, available);
+  /**
+   * Returns the content of the file specified by its name as a byte array.
+   *
+   * @param fileName the file name
+   * @return the content
+   * @throws IOException any io exception
+   * @throws NullPointerException if the specified file name is null
+   */
+  static public byte[] getFileContentAsBytes(String fileName) throws IOException, NullPointerException {
+    if (fileName == null) {
+      throw new NullPointerException("No null file name accepted");
     }
-    return output.toByteArray();
+    FileInputStream is = new FileInputStream(fileName);
+    return getStreamContentAsBytes(is);
   }
 
-  static public String getResourceAsString(String resource) throws Exception {
-    ClassLoader cl = Thread.currentThread().getContextClassLoader();
-    URL url = cl.getResource(resource);
-    InputStream is = url.openStream();
-    return getStreamContentAsString(is);
+  /**
+   * Reads a stream until its end and returns its content as a byte array. The provided
+   * stream will be closed by this method. Any runtime exception thrown when the stream is closed will
+   * be ignored and not rethrown. 
+   *
+   * @param is the input stream
+   * @return the data read from the input stream its end
+   * @throws IOException if any IOException occurs during a read
+   * @throws NullPointerException if the provided input stream is null
+   */
+  static public byte[] getStreamContentAsBytes(InputStream is) throws IOException, NullPointerException {
+    if (is == null) {
+      throw new NullPointerException("No null input stream accepted");
+    }
+    try {
+      ByteArrayOutputStream output = new ByteArrayOutputStream();
+      byte[] data = new byte[DEFAULT_BUFFER_SIZE];
+      int available;
+      while ((available = is.read(data)) > -1) {
+        output.write(data, 0, available);
+      }
+      return output.toByteArray();
+    }
+    finally {
+      Safe.close(is);
+    }
   }
 
-  static public byte[] getResourceAsBytes(String resource) throws Exception {
+  /**
+   * Get a resource from the thread context classloader and returns its content as a string. The resource
+   * is obtained by calling the method {@link java.lang.ClassLoader#getResource(String)} on the context classloader
+   * associated with the current thread of execution. The charset used for encoding the resource as a string is
+   * <code>UTF-8</code>.
+   *
+   * @param resource the resource name
+   * @return the resource content
+   * @throws NullPointerException if the specified argument is null or the loaded resource does not exist
+   * @throws IOException thrown by accessing the resource
+   */
+  static public String getResourceAsString(String resource) throws IOException {
+    byte[] bytes = getResourceAsBytes(resource);
+    return new String(bytes, "UTF-8");
+  }
+
+  /**
+   * Get a resource from the thread context classloader and returns its content as a byte array. The resource
+   * is obtained by calling the method {@link java.lang.ClassLoader#getResource(String)} on the context classloader
+   * associated with the current thread of execution.
+   *
+   * @param resource the resource name
+   * @return the resource content
+   * @throws NullPointerException if the specified argument is null or the loaded resource does not exist
+   * @throws IOException thrown by accessing the resource
+   */
+  static public byte[] getResourceAsBytes(String resource) throws IOException {
+    if (resource == null) {
+      throw new NullPointerException("Cannot provide null resource values");
+    }
     ClassLoader cl = Thread.currentThread().getContextClassLoader();
     URL url = cl.getResource(resource);
+    if (url == null) {
+      throw new NullPointerException("The resource " + resource + " was not found in the thread context classloader");
+    }
     InputStream is = url.openStream();
     return getStreamContentAsBytes(is);
   }
+
+  // Deprecated stuf ***************************************************************************************************
 
   @Deprecated
   static public byte[] serialize(Object obj) throws Exception {
@@ -112,4 +216,37 @@ public class IOUtil {
     in.close();
     return obj;
   }
+
+  /**
+   * Use {@link #getFileContentAsString(File)} instead.
+   */
+  @Deprecated
+  static public String getFileContenntAsString(File file) throws Exception {
+    return getFileContentAsString(file);
+  }
+
+  /**
+   * Use {@link #getFileContentAsString(File,String)} instead.
+   */
+  @Deprecated
+  static public String getFileContenntAsString(File file, String encoding) throws Exception {
+    return getFileContentAsString(file, encoding);
+  }
+
+  /**
+   * Use {@link #getFileContentAsString(String,String)} instead.
+   */
+  @Deprecated
+  static public String getFileContenntAsString(String fileName, String encoding) throws Exception {
+    return getFileContentAsString(fileName, encoding);
+  }
+
+  /**
+   * Use {@link #getFileContentAsString(String)} instead.
+   */
+  @Deprecated
+  static public String getFileContenntAsString(String fileName) throws Exception {
+    return getFileContentAsString(fileName);
+  }
+
 }
