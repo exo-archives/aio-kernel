4a9bcbd0e6e82614193b7a41178750864188b417
Arnaud  Héritier <arnaud.heritier@exoplatform.com>
2009-12-07 17:34:05 +0000

    KER-138: Fix/Improve tests/surefire configuration Use failsafe with run-its profile for Load/Scalability test

--- exo.kernel.component.cache/src/test/java/org/exoplatform/services/cache/test/LoadScalabilityIT.java
+++ exo.kernel.component.cache/src/test/java/org/exoplatform/services/cache/test/LoadScalabilityIT.java
@@ -37,7 +37,7 @@ import org.apache.commons.logging.Log;
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
  */
-public class TestLoadScalability extends TestCase {
+public class LoadScalabilityIT extends TestCase {
 
 
   private static void doTest(String name, List<CacheProvider> providers, int cacheSize, Config config) {
